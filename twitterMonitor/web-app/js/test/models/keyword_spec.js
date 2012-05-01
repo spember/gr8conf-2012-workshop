1971f0073a48a2c7b0bd7d8556b8236cae8a0242
Steve Pember <steve@cantinaconsulting.com>
2012-04-30 21:05:04 -0400

    Filled out the keyword model test

--- twitterMonitor/web-app/js/test/models/keyword_spec.js
+++ twitterMonitor/web-app/js/test/models/keyword_spec.js
@@ -6,9 +6,24 @@ describe('TM.Models.Keyword', function () {
             text: "gr8conf",
             numSeen: 60
         });
+        keyword.collection = {
+            defaultMax: 100
+        }
 
         it('should have the correct text', function () {
             expect(keyword.get("text")).toEqual("gr8conf");
+        });
+
+        it('should have the correct urlRoot and deleteUrl', function () {
+            expect(keyword.urlRoot).toEqual("/twitterMonitor/keyword");
+            expect(keyword.deleteURL()).toEqual("/twitterMonitor/keyword/delete/1");
+        });
+
+        it('calculates the bar width correctly', function () {
+            expect(keyword.getBarPercentage()).toEqual(60);
+            keyword.collection.maxNumSeen = 200;
+            expect(keyword.getBarPercentage()).toEqual(30);
+
         });
     });
 
