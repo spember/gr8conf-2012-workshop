90bb485afd287d262eb0ddf924ec11ca63fc8682
Steve Pember <steve@cantinaconsulting.com>
2012-05-22 13:53:30 -0400

    -Cleaned out some old Grails unit tests that weren't being used -added some relevant unit tests

--- twitterMonitor/test/unit/co/cantina/twitterMonitor/services/KeywordServiceTests.groovy
+++ twitterMonitor/test/unit/co/cantina/twitterMonitor/services/KeywordServiceTests.groovy
@@ -4,14 +4,61 @@ package co.cantina.twitterMonitor.services
 
 import grails.test.mixin.*
 import org.junit.*
+import co.cantina.twitterMonitor.Keyword
+import co.cantina.twitterMonitor.Tweet
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(KeywordService)
+@Mock([Keyword, Tweet])
 class KeywordServiceTests {
+    List keywords = []
+    Tweet tweet
+    void setUp() {
+        tweet = new Tweet(text: "I have one keyword: #gr8conf.",
+                userName: "testUser",
+                profileImageUrl: "http://test.com/profile.png",
+                id: "1234567890")
 
-    void testSomething() {
-        fail "Implement me"
+        keywords.add(new Keyword(text: "#gr8conf"))
+        keywords.add(new Keyword(text: "#blogging"))
+
     }
+
+    void testKeywordInit() {
+        // our keywords should initialize with 0 numSeen
+        System.out.println("Numseen = " +keywords[0].numSeen)
+        assertEquals 0, keywords[0].numSeen
+        assertEquals 0, keywords[1].numSeen
+    }
+
+    void testMatchKeywordToMessage() {
+        //this first should succeeed
+        assertTrue service.matchKeywordToMessage(keywords[0], tweet)
+        assertEquals 1, keywords[0].numSeen
+        assertEquals tweet.id, keywords[0].mostRecentTweet
+        // this should fail
+        assertFalse service.matchKeywordToMessage(keywords[1], tweet)
+        assertEquals 0, keywords[1].numSeen
+        assertEquals(-1, keywords[1].mostRecentTweet)
+    }
+
+    void testBadTweetInUpdateCounts() {
+        Tweet badTweet = new Tweet(userName: "testUser",
+                profileImageUrl: "http://test.com/profile.png",
+                id: "1234567891")
+        badTweet.save()
+        assertTrue badTweet.hasErrors()
+        service.updateCounts(badTweet, keywords)
+        assertFalse badTweet.processed
+        badTweet.text = "#blogging takes a lot of time"
+        badTweet.save()
+
+        service.updateCounts(badTweet, keywords)
+        assertTrue(badTweet.processed)
+        assertEquals(1, keywords[1].numSeen)
+    }
+
+
 }
--- twitterMonitor/test/unit/co/cantina/twitterMonitor/services/TweetServiceTests.groovy
+++ twitterMonitor/test/unit/co/cantina/twitterMonitor/services/TweetServiceTests.groovy
@@ -3,14 +3,53 @@ package co.cantina.twitterMonitor.services
 
 
 import grails.test.mixin.*
+import co.cantina.twitterMonitor.Tweet
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(TweetService)
+@Mock(Tweet)
 class TweetServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testSaveFromJSON() {
+        def params = [
+                text:"This is a sample tweet for #gr8conf",
+                profile_image_url: "http://www.test.com/images/profile.png",
+                from_user_name: "@gr8conf",
+        ]
+
+        // bad creation
+
+        Tweet tweet = service.saveFromJSON(params)
+        tweet.save()
+        assertNull(tweet.id)
+
+        params.id = 1234567890l
+
+        tweet = service.saveFromJSON(params)
+        tweet.save()
+        assertFalse(tweet.hasErrors())
+
+    }
+
+    void testRemoveOldMessages() {
+        assertEquals 0, Tweet.list().size()
+
+        Tweet tweet = new Tweet(text: "I have one keyword: #gr8conf.",
+                userName: "testUser",
+                profileImageUrl: "http://test.com/profile.png",
+                id: "1234567890")
+        tweet.save()
+
+        assertEquals 1, Tweet.list().size()
+
+        tweet.dateCreated = new Date()-10
+        tweet.save()
+
+
+        service.removeOldMessages()
+        assertEquals 0, Tweet.list().size()
+
     }
 }
